dcf84ee84b426d5f6c6cbd89c9fe748cf7f77e71
jacek <jaco04@wp.pl>
2018-10-16 21:19:13 +0200

    Continuing with the project. Contructor injected. Service and its impl added. @Data from lobok added.

---
+++ todo-list/src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -0,0 +1,53 @@
+package academy.learnprogramming.controller;
+
+import academy.learnprogramming.service.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+
+    // == fields ==
+    private final DemoService demoService;
+
+    // == constructors ==
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    // == request methods ==
+
+    //http//localhost:8080/todo-list/hello
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return demoService.getWelcomeMessage();
+    }
+
+    //http//localhost:8080/todo-list/welcome
+    //http//localhost:8080/todo-list/welcome?user=Jacek
+    //http//localhost:8080/todo-list/welcome?user=Jacek&age=31
+    @GetMapping("welcome")
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("userName", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
+        log.info("model = {}", model);
+        return "welcome";
+    }
+
+    // == model attributes ==
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return demoService.getWelcomeMessage();
+    }
+
+}
