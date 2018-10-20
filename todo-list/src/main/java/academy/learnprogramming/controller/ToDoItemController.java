8a3b4f51f3fd2737a497201b9885edfa74a88c71
jacek <jaco04@wp.pl>
2018-10-20 12:26:55 +0200

    Delete, Edit and Update mappings added.

--- todo-list/src/main/java/academy/learnprogramming/controller/ToDoItemController.java
+++ todo-list/src/main/java/academy/learnprogramming/controller/ToDoItemController.java
@@ -1,14 +1,23 @@
 package academy.learnprogramming.controller;
 
 import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
 import academy.learnprogramming.service.ToDoItemServiceImpl;
+import academy.learnprogramming.util.AttributeNames;
 import academy.learnprogramming.util.Mappings;
 import academy.learnprogramming.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class ToDoItemController {
 
@@ -33,5 +42,39 @@ public class ToDoItemController {
         return ViewNames.ITEMS_LIST;
     }
 
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id,
+                              Model model) {
+        log.info("Editing id = {}", id);
+        TodoItem todoItem = toDoItemService.getItem(id);
+
+        if (todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+
+        }
+
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
+        log.info("todoItem from form= {}", todoItem);
+
+        if (todoItem.getId() == 0) {
+            toDoItemService.addItem(todoItem);
+        } else {
+            toDoItemService.updateItem(todoItem);
+        }
+
+        return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        log.info("Deleting item with id= {}", id);
+        toDoItemService.removeItem(id);
+        return "redirect:/" + Mappings.ITEMS;
+    }
 
 }
